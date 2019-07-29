b004f7d4fa3a57c70e16ded68a862702daeed400
Emma Sharon <emmasharon19@gmail.com>
2019-07-29 11:27:03 +0300

    create form for adding post and creating profile

---
+++ insta/forms.py
@@ -0,0 +1,12 @@
+from .models import Profile,Post
+from django import forms
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['profile','date','like']
